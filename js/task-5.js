a09d3eeedf41d7eef00bd6598de9093e265060ac
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2024-07-19 23:03:34 +0200

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+//Для генерування випадкового кольору-функція getRandomHexColor()
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+//визначаємо елементи, з якими буде працювати скрипт.
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+//click- змінює колір фону при кожному кліку.
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Функція для створення колекції елементів
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  // Очищення контейнера перед додаванням нових елементів
+  boxesContainer.innerHTML = "";
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.marginBottom = "5px";
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+// Функція для очищення колекції елементів
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+// Додавання обробників подій для кнопок
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.querySelector("button[data-create]");
+  const destroyButton = document.querySelector("button[data-destroy]");
+  const inputField = document.querySelector("#controls input");
+
+  createButton.addEventListener("click", () => {
+    const amount = parseInt(inputField.value.trim());
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      inputField.value = ""; // Очищення значення інпуту після створення елементів
+    } else {
+      alert("Please enter a number between 1 and 100.");
+    }
+  });
+
+  destroyButton.addEventListener("click", destroyBoxes);
+});
